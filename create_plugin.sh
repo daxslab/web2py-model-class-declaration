80a6ed032636ce9fb2657656277c24dcd8787bce
Carlos Cesar Caballero Díaz <desarrollo@spicm.cfg.sld.cu>
2015-11-02 11:48:12 -0500

    new plugin name

--- create_plugin.sh
+++ create_plugin.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env sh
 
-rm web2py.plugin.dal_class_declaration.w2p
-tar czvf web2py.plugin.dal_class_declaration.w2p --exclude='*.sh'  --exclude='*.md' --exclude='LICENSE' --exclude='*.pyc' *
+rm web2py.plugin.model_class_declaration.w2p
+tar czvf web2py.plugin.model_class_declaration.w2p --exclude='*.sh'  --exclude='*.md' --exclude='LICENSE' --exclude='*.pyc' *
